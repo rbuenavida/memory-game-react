2c1f66d809ea3a2f8849d3c4b0c1e06e2019481a
ronb <rbuenavida@gmail.com>
2017-12-09 12:17:20 +0200

    + Added css module loading

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,13 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
       { 
         test: /\.js$/, 
         exclude: /node_modules/, 
